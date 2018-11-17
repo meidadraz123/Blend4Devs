fd0a1ad06a0c0f109921da48e397f12d5b4c7b84
meidad raz <meidad.raz@gmail.com>
2018-11-17 20:12:30 +0200

    Course material - before starting the course

---
+++ ch03 layout/Code/after/DesignWithBlend/DesignWithBlend/MainPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+
+namespace DesignWithBlend
+{
+	public partial class MainPage : UserControl
+	{
+		public MainPage()
+		{
+			// Required to initialize variables
+			InitializeComponent();
+		}
+
+		private void theButton_MouseEnter(object sender, System.Windows.Input.MouseEventArgs e)
+		{
+      LayoutRoot.Background = new SolidColorBrush(Colors.Gray);
+		}
+
+		private void theButton_MouseLeave(object sender, System.Windows.Input.MouseEventArgs e)
+		{
+      LayoutRoot.Background = new SolidColorBrush(Colors.White);
+		}
+	}
+}
---
+++ ch05 platforms/Code/after/MyFirstSketchflow/MyFirstSketchflow/MyFirstSketchflowScreens/Third_Screen.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+
+namespace MyFirstSketchflowScreens
+{
+	public partial class Third_Screen : UserControl
+	{
+		public Third_Screen()
+		{
+			// Required to initialize variables
+			InitializeComponent();
+		}
+	}
+}

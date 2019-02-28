f7360780c4ec20d1cb69ee6c014eb11cfb47031f
meidad raz <meidad.raz@gmail.com>
2019-02-28 17:06:11 +0200

    CH04 - refactoring into user control

---
+++ ch04 advanced/demos/before/AdvancedBlendWpf/AdvancedBlendWpf/GamesEditor.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AdvancedBlendWpf
+{
+    /// <summary>
+    /// Interaction logic for GamesEditor.xaml
+    /// </summary>
+    public partial class GamesEditor : UserControl
+    {
+        public GamesEditor()
+        {
+            InitializeComponent();
+        }
+    }
+}
